5917f0da412f075107b550be91500ea11853b325
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-04-08 20:22:29 -0400

    added function to read

--- src/music.js
+++ src/music.js
@@ -9,3 +9,10 @@ export async function addSong(req, res) {
     await coll.insertOne(newSong)
     res.status(201).send("New song added")
 }
+
+// READ 
+
+export async function getSongs ( req, res) {
+    const allSongs = await coll.find({}).toArray()
+    res.status(200).send(allSongs)
+}
